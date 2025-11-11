03e8e9ac4de2ecd17ccd23526451d60b8b6bc3a8
gurukondaveeti <gurukondaveeti@gmail.com>
2025-11-11 15:54:00 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { PORT} = require('./config');
+const app= express();
+app.listen(PORT, () =>{
+console.log(`Successfully started the server on PORT: ${PORT}`);
+});
